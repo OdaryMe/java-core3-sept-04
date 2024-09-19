4ca91a9d9cc9a79e0c2cbffb3430cfde48b3c730
OdaryMe <elenanevsk@gmail.com>
2024-09-19 18:38:03 +0300

    db

--- src/main/java/ru/geekbrains/java_core3/lessons/l2_jdbc/Database.java
+++ src/main/java/ru/geekbrains/java_core3/lessons/l2_jdbc/Database.java
@@ -8,7 +8,8 @@ public class Database {
     private static PreparedStatement ps;
     private static String insertStatement = "insert into users (nickname, password) values (?, ?);";
     private static final String DB_CONNECTION_STRING = "jdbc:sqlite:db/example.db";
-    private static final String CREATE_REQUEST = "create table if not exists users (id integer primary key, autoincrement, nickname text, password text);";
+    private static final String CREATE_REQUEST = "create table if not exists users (id integer primary key autoincrement, nickname text, password text);";
+    private static final String DROP_REQUEST = "drop table if exists users;";
 
     public static void main(String[] args) {
         try {
@@ -19,12 +20,29 @@ public class Database {
             preparedStatement("Scorpion", "someone");
             preparedStatement("Henrietta", "zuyd");
             massInsertBatch();
+//            dropAllTable();
+            simpleRead();
         }
         catch (SQLException e) {
             e.printStackTrace();
         } finally {
             disconnect();
         }
+    }
+
+    private static void dropAllTable() throws SQLException {
+        statement.execute(DROP_REQUEST);
+    }
+
+    private static void simpleRead() throws SQLException {
+       try (var resultSet = statement.executeQuery("select * from users")) {
+           while (resultSet.next()) {
+               System.out.printf("User id: %d, nickname: %s, password: %s\n",
+                       resultSet.getInt(1),
+                       resultSet.getString("nickname"),
+                       resultSet.getString("password"));
+           }
+       }
     }
 
     private static void preparedStatement(String nickname, String password) throws SQLException {
@@ -36,7 +54,7 @@ public class Database {
 
     private static void massInsertBatch() throws SQLException {
         connection.setAutoCommit(false);
-        for (int i = 1; i <= 15; i++) {
+        for (int i = 1; i <= 5; i++) {
             ps.setInt(2, i);
             ps.setString(1, "User #" + i);
             ps.addBatch();
@@ -48,7 +66,7 @@ public class Database {
     private static void connect() throws SQLException {
         connection = DriverManager.getConnection(DB_CONNECTION_STRING);
         statement = connection.createStatement();
-        ps = connection.prepareStatement(insertStatement);
+//        ps = connection.prepareStatement(insertStatement);
     }
 
     private static void createTable() throws SQLException {
